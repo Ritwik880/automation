5ec4ad1efed66228b9c7347805cf0de50e398f60
ritwik <ritwik@guise.ai>
2024-09-06 09:29:03 +0530

    Added generating test reports package

--- README.md
+++ README.md
@@ -6,17 +6,17 @@ This project demonstrates JavaScript automation using Selenium WebDriver for fro
 
 1. **Prerequisites:** Ensure you have Node.js and npm (or yarn) installed. Verify this using `node -v` and `npm -v` (or `yarn -v`). If not installed, download them from https://nodejs.org/.
 
-3. **Running Frontend Automation test cases:**
+3. **Running Frontend Automation test cases and generating test report:**
     ```bash
      cd frontend_automation
     ```
-   - Run `npm install selenium-webdriver`
-   - Run `node automation.js`
+   - Run `npm install selenium-webdriver mocha chai mochawesome`
+   - Run `npx mocha .\automation.mjs --reporter mochawesome --reporter-options reportDir=./reports reportFilename=automation-test-report`
 
-3. **Running Backend Automation API test cases:**
+3. **Running Backend Automation API test cases and generating test report:**
     ```bash
      cd backend_automation
     ```
    - Run `npm init -y` (or `yarn init -y`) to create a `package.json` file.
-   - Run `npm install mocha chai axios`
-   - Run `npx mocha .\api.test.mjs`
+   - Run `npm install mocha chai axios mochawesome`
+   - Run `npx mocha .\api.test.mjs --reporter mochawesome --reporter-options reportDir=./reports reportFilename=automation-test-report`
--- frontend_automation/automation.mjs
+++ frontend_automation/automation.mjs
@@ -1,9 +1,20 @@
-const { Builder, By, until } = require('selenium-webdriver');
+// const { Builder, By, until } = require('selenium-webdriver');
+import { Builder, By, until } from 'selenium-webdriver';
+import { expect } from 'chai';
 
-async function automateProduct() {
+describe('shopping in amazon', function () {
+    this.timeout(30000);
+    let driver;
+
+    before(async function () {
+        driver = await new Builder().forBrowser('chrome').build();
+    });
+
+    after(async function () {
+        await driver.quit();
+    });
     
-    let driver = await new Builder().forBrowser('chrome').build();
-    try {
+    it('should be able to search for a product and get that product detail', async function () {
         await driver.manage().setTimeouts({ implicit: 30000 });
         await driver.get('https://www.amazon.in/');
 
@@ -29,24 +40,18 @@ async function automateProduct() {
         console.log(`Product Name: ${getFirstProductName}`);
         console.log(`Product Price: ${getFirstProductPrice}`);
         console.log(`Product Link: ${getProductLink}`);
+    });
 
+    it('should be able to add the product to the cart and proceed to buy', async function() {
         await driver.get('https://www.amazon.in/Campus-North-Plus-Running-Shoes/dp/B08PRY7ZY8/ref=sr_1_2_sspa?crid=14W68V78Y30PB&dib=eyJ2IjoiMSJ9.Lqk9sXXwyNIgnRct0FKs0fhoIptQXCkIkNzceu5JLfvEOjh7cshnPNjDHuMVmTGT6gcyDT_0ATQeffu3sxrcKTXUhH6IGqT7FgQ_nuA75D4EXQIZKO6ATPt6jkz3EeVIsScyeLSP04ApmDcPgDjVdrjUSIwDcviRCbBwbWQ51L-vhuEaOppCZ5LwmluF2FRiUKEFMQJQstt7I9Oxy8clAiv4g0pZO43DZYfmLJ0nB-YCO3KCS06yu1Zq3_kVPid-aJxPiNHx7wfbB6ww0SPKkJC1l5dsgM3bQpFPfhnK0hA.xHCchL4N2o4Td6z98AZU3R5uNN8msuMXYL7bHdjOpUc&dib_tag=se&keywords=Campus+shoe&nsdOptOutParam=true&qid=1725591026&sprefix=campus+sho%2Caps%2C249&sr=8-2-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1');
 
         let clickOnAddToCart = await driver.findElement(By.xpath('//input[@id="add-to-cart-button"]'));
         await clickOnAddToCart.click();
 
-        await driver.wait(until.elementLocated(By.xpath('//a[@id="nav-cart"]')));
-        await driver.findElement(By.xpath('//a[@id="nav-cart"]')).click()
+        await driver.wait(until.elementLocated(By.xpath('//a[@id="nav-cart"]')), 10000);
+        await driver.findElement(By.xpath('//a[@id="nav-cart"]')).click();
 
-        await driver.wait(until.elementLocated(By.xpath('//span[@id="sc-buy-box-ptc-button"]')), 5000);
-
-        //since we have to login first to navigate to the checkout page, so I have kept till this here only.
-        let proceedToBuy = await driver.findElement(By.xpath('//span[@id="sc-buy-box-ptc-button"]'));
-        await proceedToBuy.click();
-
-    }
-    finally {
-        await driver.quit()
-    }
-}
-automateProduct()
+        let proceedToBuy = await driver.wait(until.elementLocated(By.xpath('//span[@id="sc-buy-box-ptc-button"]')), 5000);
+        expect(proceedToBuy).to.exist;
+    });
+})
